dccc963dd5b4a985c6288f260531c93753dcce75
shaifimran <shaif.imran@gmail.com>
2023-08-16 15:35:22 -0400

    task 1 completed

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,10 +1,25 @@
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { budget,dispatch } = useContext(AppContext);
+    const changeBudget = (num) => {
+        if(num>=0){
+        dispatch({
+            type: 'SET_BUDGET',
+            payload: parseInt(num),
+        });
+    }
+};
     return (
         <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+            <span>Budget: £<input required='required'
+                        type='number'
+                        id='cost'
+                        value={budget}
+                        style={{ marginLeft: '2rem' , size: 10}}
+                        onChange={(event) => changeBudget(event.target.value)}
+                        
+             ></input></span>
         </div>
     );
 };
